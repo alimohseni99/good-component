012e8ef56b85b32d7646bc4bcf21f488e3e34783
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-10-28 11:32:22 +0100

    added icon with hard coded names

--- src/component/link-button.tsx
+++ src/component/link-button.tsx
@@ -1,15 +1,30 @@
-type LinkProps = {
-  url: string;
-  name: string;
-};
+import { SiLinkedin, SiGithub } from "react-icons/si";
 type SocialMedia = "Linkedin" | "GitHub";
 
+type LinkProps = {
+  url: string;
+  name: SocialMedia;
+};
 
 export const LinkButton = ({ url, name }: LinkProps) => {
+  let Icon = null;
+
+  if (name === "Linkedin") {
+    Icon = SiLinkedin;
+  } else if (name === "GitHub") {
+    Icon = SiGithub;
+  } else {
+    console.log("Unkown name");
+    return null;
+  }
+
   return (
     <div>
       <button className="link-button">
-        <a href={url}>{name}</a>
+        <a href={url}>
+          {name}
+          {Icon && <Icon style={{ marginLeft: "8px" }} />}
+        </a>
       </button>
     </div>
   );
